e7557fb096ecd73b251163428fef83c385bc0362
EslamSallam <eslam.salam1369@gmail.com>
2022-05-03 20:18:53 +0200

    Building User Interface

--- WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
+++ WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace WiredBrainCoffee.CustomersApp
 {
@@ -15,6 +16,19 @@ namespace WiredBrainCoffee.CustomersApp
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //BtnAddCustomer.Content = "Customer Added!";
+        }
+
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            //var Col = CustomerListGrid.GetValue(Grid.ColumnProperty);
+            //if ((int)Col == 0)
+            //    CustomerListGrid.SetValue(Grid.ColumnProperty, 2);
+            //else
+            //    CustomerListGrid.SetValue(Grid.ColumnProperty, 0);
+            var col = Grid.GetColumn(CustomerListGrid);
+            var newCol = col == 0 ? 2 : 0;
+            Grid.SetColumn(CustomerListGrid, newCol);
         }
     }
 }
